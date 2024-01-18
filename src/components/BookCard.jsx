86fe7b9d6d4391889fcceb4b574b24d58ff486a1
RF-Rifat <rfrifat6344@gmail.com>
2024-01-19 00:57:04 +0600

    buttom color change color function added

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,17 +1,19 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
 const BookCard = ({ cardData }) => {
+  const [color, setColor] = useState(false);
   console.log(cardData);
   const { name, author, price } = cardData;
+  const handleFavouriteClick = () => {
+    setColor(!color);
+  };
   return (
     <>
       <div className="space-y-3">
         {/* thumbnail */}
         <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4">
-          <img
-            className="max-w-[144px]"
-            src="./book.png"
-            alt="book name"
-          />
+          <img className="max-w-[144px]" src="./book.png" alt="book name" />
         </div>
         {/* info */}
         <div className="space-y-3">
@@ -49,7 +51,12 @@ const BookCard = ({ cardData }) => {
               </svg>
               Add to Cart
             </button>
-            <button className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%] lg:py-1.5">
+            <button
+              onClick={handleFavouriteClick}
+              className={`flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all lg:py-1.5  ${
+                color ? "bg-red-500" : "bg-[#1C4336]"
+              }`}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
